2cdb94959bb117bf0910a15baa65760f5a53b509
Andrew Aleynikov <drew.lake@yandex.ru>
2020-02-25 20:59:04 +0300

    add: telegram bot, connection

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -3,6 +3,7 @@ package telegram;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -11,14 +12,17 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            new Thread(
+                    () -> handleMessage(update.getMessage())
+            ).start();
+        }
+    }
+
+    private void handleMessage(Message message) {
+        try {
+            execute(new SendMessage().setChatId(message.getChatId()).setText(Thread.currentThread().getName()));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
