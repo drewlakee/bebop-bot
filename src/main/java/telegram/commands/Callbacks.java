d069d67fea0aeab0aa1e080b2292cd2b8143d02c
Andrew Aleynikov <drew.lake@yandex.ru>
2020-03-14 19:32:45 +0300

    upd: rename /random callbacks

--- src/main/java/telegram/commands/Callbacks.java
+++ src/main/java/telegram/commands/Callbacks.java
@@ -2,9 +2,9 @@ package telegram.commands;
 
 public class Callbacks {
 
-    public final static String CHOOSE_GROUP = "Выбери группу, в которую хочешь пост:";
+    public final static String CHOOSE_GROUP_RANDOM_COMMAND = "Выбери группу, в которую хочешь случайный пост:";
 
-    public final static String CHOOSE_POST = "Выбери пикчу";
+    public final static String CHOOSE_POST_RANDOM_COMMAND = "Самостоятельный подбор контента для случайного поста";
 
-    public final static String ASK_CHOOSE_POST = "Хочешь подобрать слуйчайный пост?";
+    public final static String ASK_CHOOSE_POST_RANDOM_COMMAND = "Хочешь подобрать слуйчайный пост?";
 }
