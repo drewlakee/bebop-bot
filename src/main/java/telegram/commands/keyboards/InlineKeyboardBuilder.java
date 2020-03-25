ac6037582e0b471b97a5f0b85474706f9807018a
Andrew Aleynikov <drew.lake@yandex.ru>
2020-03-25 19:51:50 +0300

    upd: keyboard builder

--- src/main/java/telegram/commands/keyboards/InlineKeyboardBuilder.java
+++ src/main/java/telegram/commands/keyboards/InlineKeyboardBuilder.java
@@ -1,5 +1,35 @@
 package telegram.commands.keyboards;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InlineKeyboardBuilder {
-    // TODO
+    private InlineKeyboardMarkup keyboardMarkup;
+    private List<List<InlineKeyboardButton>> buttons;
+    private int line;
+
+    InlineKeyboardBuilder() {
+        this.keyboardMarkup = new InlineKeyboardMarkup();
+        this.buttons = new ArrayList<>();
+        this.line = 0;
+        this.buttons.add(new ArrayList<>());
+    }
+
+    public InlineKeyboardBuilder addButton(InlineKeyboardButton button) {
+        buttons.get(line).add(button);
+        return this;
+    }
+
+    public InlineKeyboardBuilder nextLine() {
+        this.line++;
+        this.buttons.add(new ArrayList<>());
+        return this;
+    }
+
+    public InlineKeyboardMarkup build() {
+        return keyboardMarkup.setKeyboard(buttons);
+    }
 }
