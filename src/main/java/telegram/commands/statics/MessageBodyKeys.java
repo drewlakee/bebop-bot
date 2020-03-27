e2f4ee130fc84031e8a820a9aed7dd2a4fdb6779
Andrew Aleynikov <drew.lake@yandex.ru>
2020-03-28 00:20:16 +0300

    upd: message body keys

--- src/main/java/telegram/commands/statics/MessageBodyKeys.java
+++ src/main/java/telegram/commands/statics/MessageBodyKeys.java
@@ -2,8 +2,8 @@ package telegram.commands.statics;
 
 public class MessageBodyKeys {
 
-    private final static String MODE = "Режим";
+    public final static String MODE = "Режим";
 
-    private final static String MANUAL_MODE = MODE + ": " + "ручной";
-    private final static String RANDOM_MODE = MODE + ": " + "рандом";
+    public final static String MANUAL_MODE = MODE + ": " + "ручной";
+    public final static String RANDOM_MODE = MODE + ": " + "рандом";
 }
