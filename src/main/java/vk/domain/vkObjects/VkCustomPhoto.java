4ba77f5a0f6a98a321cd6cbe3831f7f4a5d950c8
Andrew Aleynikov <drew.lake@yandex.ru>
2020-04-28 16:06:39 +0300

    add: largest size method

--- src/main/java/vk/domain/vkObjects/VkCustomPhoto.java
+++ src/main/java/vk/domain/vkObjects/VkCustomPhoto.java
@@ -1,8 +1,13 @@
 package vk.domain.vkObjects;
 
 import com.vk.api.sdk.objects.photos.Photo;
+import com.vk.api.sdk.objects.photos.PhotoSizes;
 
 public class VkCustomPhoto extends Photo implements VkAttachment {
+
+    public PhotoSizes getLargestSize() {
+        return this.getSizes().get(this.getSizes().size() - 1);
+    }
 
     @Override
     public String toAttachmentString() {
