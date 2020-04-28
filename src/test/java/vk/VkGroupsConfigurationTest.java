c683a141fa3021968d36f3477c69cdae5d594d04
Andrew Aleynikov <drew.lake@yandex.ru>
2020-04-28 16:06:57 +0300

    upd: names

--- src/test/java/vk/VkGroupsConfigurationTest.java
+++ src/test/java/vk/VkGroupsConfigurationTest.java
@@ -4,7 +4,7 @@ import com.google.common.collect.ComparisonChain;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.Test;
-import vk.domain.groups.GroupObjective;
+import vk.domain.groups.VkGroupObjective;
 import vk.domain.groups.VkCustomGroup;
 import vk.services.VkGroupsConfigurationService;
 
@@ -19,12 +19,12 @@ public class VkGroupsConfigurationTest {
 
     @Test
     public void testGroupLoadingFromFile() throws IOException {
-        GroupObjective groupObjective = GroupObjective.values()[ThreadLocalRandom.current().nextInt(GroupObjective.values().length)];
+        VkGroupObjective vkGroupObjective = VkGroupObjective.values()[ThreadLocalRandom.current().nextInt(VkGroupObjective.values().length)];
         String name = RandomStringUtils.randomAlphabetic(10);
         int groupId = ThreadLocalRandom.current().nextInt(-100000, 100000);
         String url = "https://vk.com/id" + groupId;
         String[] groupAttributes = new String[] {
-                "<" + groupObjective.toString() + ">",
+                "<" + vkGroupObjective.toString() + ">",
                 "<" + name + ">",
                 "<" + groupId + ">",
                 "<" + url + ">"
@@ -40,7 +40,7 @@ public class VkGroupsConfigurationTest {
         VkCustomGroup group = groupsFromFile.get(name);
 
         Assert.assertEquals(0, ComparisonChain.start()
-                .compare(groupObjective, group.getGroupObjective())
+                .compare(vkGroupObjective, group.getVkGroupObjective())
                 .compare(name, group.getName())
                 .compare(groupId, group.getId().intValue())
                 .compare(url, group.getUrl())
