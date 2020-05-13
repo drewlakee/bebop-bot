896c6a06abc8393b47aaacc7efc2a22ad9c4cd81
Andrew Aleynikov <drew.lake@yandex.ru>
2020-05-13 03:02:26 +0300

    upd: groups desk

--- src/main/java/telegram/commands/MyGroupsCommand.java
+++ src/main/java/telegram/commands/MyGroupsCommand.java
@@ -6,7 +6,7 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 import telegram.commands.handlers.BotCommand;
 import telegram.ResponseMessageDispatcher;
 import telegram.commands.handlers.MessageHandler;
-import telegram.commands.statics.Commands;
+import telegram.commands.handlers.Commands;
 import vk.domain.groups.VkCustomGroup;
 import vk.domain.groups.VkGroupPool;
 
@@ -31,11 +31,7 @@ public class MyGroupsCommand extends BotCommand implements MessageHandler {
         int count = 0;
 
         for (VkCustomGroup group : VkGroupPool.getHostGroups()) {
-            desk.append("\n\n")
-                .append(count + ": " + group.getName() + " (" + group.getId() + ")")
-                .append("\n")
-                .append(group.getUrl());
-
+            desk.append(String.format("\n\n%d: %s (%d)\n%s", count, group.getName(), group.getId(), group.getUrl()));
             count++;
         }
         desk.append("\n\n").append("Total - ").append(count).append(".");
