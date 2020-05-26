a810f857e073b318bce9e50c0bfb77ca0f625f0d
Andrew Aleynikov <drew.lake@yandex.ru>
2020-05-27 00:13:25 +0300

    add: new send method

--- src/main/java/github/drewlakee/telegram/utils/ResponseMessageDispatcher.java
+++ src/main/java/github/drewlakee/telegram/utils/ResponseMessageDispatcher.java
@@ -4,11 +4,20 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageMedia;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class ResponseMessageDispatcher {
+
+    public static void send(AbsSender sender, EditMessageReplyMarkup editMessageReplyMarkup) {
+        try {
+            sender.execute(editMessageReplyMarkup);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void send(AbsSender sender, SendMessage sendMessage) {
         try {
